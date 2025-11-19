92ab20c2e0af5b55ff5e57f1fc84099c6e9cc322
BenilsonFalcao <benilson.25104708360010@faeterj-rio.edu.br>
2025-11-19 09:21:22 -0300

    Update and rename corpo.html to config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// Configurações do Banco de Dados
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'sistema_monitoramento');
+define('DB_USER', 'root'); // Geralmente 'root' no XAMPP/WAMP
+define('DB_PASS', '');     // Geralmente vazio no XAMPP
+?>
